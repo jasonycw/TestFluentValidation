1066df7c1f2d53d14c95bca508ad66cbd464cd5d
Jason Yu <jason_yu@executivecentre.com>
2019-02-28 15:21:11 +0800

    🤔

---
+++ TestFluentValidation/Validators/ValidatorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using TestFluentValidation.Domain;
+
+namespace TestFluentValidation.Validators
+{
+    public static class ValidatorExtensions
+    {
+        public static void EmailAddress<T>(this IRuleBuilder<T, string> rule, bool acceptTecEmail)
+        {
+            rule.Must(x => Email.Create(x) != null)
+                .WithState(x => new Error("EmailValidator", $"{x} is null"));
+            rule.Must(x => Email.Create(x)?.IsGmail ?? false)
+                .When(x => !acceptTecEmail)
+                .WithState(x => new Error("EmailValidator", $"{Email.Create(x)} is null"));
+
+        }
+    }
+}
